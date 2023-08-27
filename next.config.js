f60d298ded937bc3a4006c54e5b0fe09555d8b28
Ellie Bridges <elliebridges@Ellies-MacBook-Pro.local>
2023-08-27 16:57:16 -0600

    adjusted next.config to address we bpacks error

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
         output: 'export',
 }
 
-
-
-module.exports = nextConfig
+module.exports = { 
+        nextConfig,
+        webpack: (config, { isServer }) => {
+          if (!isServer) {
+            config.resolve.fallback.fs = false;
+          }
+          return config;
+        },
+      }
