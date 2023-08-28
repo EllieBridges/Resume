14895b667f7f54ac380150a6c5f4837df3f024f8
Ellie Bridges <elliebridges@Ellies-MacBook-Pro.local>
2023-08-28 11:48:00 -0600

    imported pictures to speed up load time and installed npm sharp for file conversion

--- app/gallery/PhotoGallery.tsx
+++ app/gallery/PhotoGallery.tsx
@@ -1,92 +1,120 @@
-import Image from "next/image"
+import Image from "next/image";
+import walt from '../../public/walt.png'
+import pony from '../../public/pony.png'
+import crow from '../../public/crow.png'
+import elf from '../../public/elf.png'
+import whale from '../../public/whale.png'
+import owl from '../../public/owl.png'
+import dog from '../../public/dog.png'
+import bouquet from '../../public/bouquet.png'
+import gannet from '../../public/gannet.png'
+import monster from '../../public/monster.png'
+import fish from '../../public/fish.png'
+import antelope from '../../public/antelope.png'
+
+// const imageLoader = ({ src, width, quality }) => {
+//     return `https://example.com/${src}?w=${width}&q=${quality || 75}`
+// }
 
 const PhotoGallery = () => {
     return (
-        <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
+        <div className='container mx-auto px-5 py-2 lg:px-32 lg:pt-24 mb-40'>
 
-            <div className="flex flex-wrap flex-col md:flex-row md:-m-2">
+            <div className='flex flex-wrap flex-col md:flex-row md:-m-2'>
 
-                <div className='flex w-1/2 flex-wrap mx-auto w-full md:w-1/2'>
+                <div className='flex flex-wrap mx-auto w-full md:w-1/2'>
                     <picture className='w-1/2 p-1 md:p-2'>
-                        <img
-                            alt='gallery'
-                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-150'
-                            src='walt.png' />
+                        <Image
+                            alt='A drawing of Walter White with a diamond earring'
+                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-125 md:hover-110'
+                            src={walt}
+                            placeholder='blur' />
                     </picture>
                     <picture className='w-1/2 p-1 md:p-2'>
-                        <img
-                            alt='gallery'
-                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-150'
-                            src='pony.png' />
+                        <Image
+                            alt='A drawing of a dappled pony woth a pink background'
+                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-125 md:hover-110'
+                            src={pony}
+                            placeholder='blur' />
                     </picture >
                     <picture className='w-full p-1 md:p-2'>
-                        <img
-                            alt='gallery'
-                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-150'
-                            src='crow.png' />
+                        <Image
+                            alt='A drawing of a crow holding a sink plug'
+                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-125 md:hover-110'
+                            src={crow}
+                            placeholder='blur' />
                     </picture >
                 </div>
 
-                <div className='flex w-1/2 flex-wrap mx-auto w-full md:w-1/2'>
+                <div className='flex flex-wrap mx-auto w-full md:w-1/2'>
                     <picture className='w-full p-1 md:p-2'>
-                        <img
-                            alt='gallery'
-                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-150'
-                            src='elf.png' />
+                        <Image
+                            alt='A drawing of a disappointed elf'
+                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-125 md:hover-110'
+                            src={elf}
+                            placeholder='blur' />
                     </picture >
                     <picture className='w-1/2 p-1 md:p-2'>
-                        <img
-                            alt='gallery'
-                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-150'
-                            src='whale.png' />
+                        <Image
+                            alt='A drawing of the underside of a whale'
+                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-125 md:hover-110'
+                            src={whale}
+                            placeholder='blur' />
                     </picture >
                     <picture className='w-1/2 p-1 md:p-2'>
-                        <img
-                            alt='gallery'
-                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-150'
-                            src='owl.png' />
+                        <Image
+                            alt='A drawing of a barn owl in front of the sun'
+                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-125 md:hover-110'
+                            src={owl}
+                            placeholder='blur' />
                     </picture >
                 </div>
 
-                <div className='flex w-1/2 flex-wrap mx-auto w-full md:w-1/2'>
+                <div className='flex flex-wrap mx-auto w-full md:w-1/2'>
                     <picture className='w-1/2 p-1 md:p-2'>
-                        <img
-                            alt='gallery'
-                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-150'
-                            src='dog.png' />
+                        <Image
+                            alt='A drawing of a cocker spaniel'
+                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-125 md:hover-110'
+                            src={dog}
+                            placeholder='blur' />
                     </picture>
                     <picture className='w-1/2 p-1 md:p-2'>
-                        <img
-                            alt='gallery'
-                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-150'
-                            src='bouquet.png' />
+                        <Image
+                            alt='A drawing of a dried flower bouquet'
+                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-125 md:hover-110'
+                            src={bouquet}
+                            placeholder='blur' />
                     </picture>
                     <picture className='w-full p-1 md:p-2'>
-                        <img
-                            alt='gallery'
-                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-150'
-                            src='/gannet.png' />
+                        <Image
+                            alt='A drawing of a pair of gannets courting'
+                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-125 md:hover-110'
+                            src={gannet}
+                            placeholder='blur' />
                     </picture>
                 </div>
 
-                <div className='flex w-1/2 flex-wrap mx-auto w-full md:w-1/2'>
+                <div className='flex flex-wrap mx-auto w-full md:w-1/2'>
                     <picture className='w-1/2 p-1 md:p-2'>
-                        <img
-                            alt='gallery'
-                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-150'
-                            src='monster.png' />
+                        <Image
+                            alt='A drawing of a monster with horns in front of a swirling background'
+                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-125 md:hover-110'
+                            src={monster}
+                            placeholder='blur' />
                     </picture>
                     <picture className='w-1/2 p-1 md:p-2'>
-                        <img
-                            alt='gallery'
-                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-150'
-                            src='fish.png' />
+                        <Image
+                            alt='A drawing of a koi fish swimming from above'
+                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-125 md:hover-110'
+                            src={fish}
+                            placeholder='blur' />
                     </picture>
                     <picture className='w-full p-1 md:p-2'>
-                        <img
-                            alt='gallery'
-                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-150'
-                            src='antelope.png' />
+                        <Image
+                            alt='A drawing of a party antelope covered in silly string and streamers'
+                            className='block h-full w-full rounded-lg object-cover object-center hover:scale-125 md:hover-110'
+                            src={antelope}
+                            placeholder='blur' />
                     </picture>
                 </div>
 
--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -8,9 +8,9 @@ const Page = () => {
       <Header
         text='Gallery' />
 
-      <h1 className='font-primary text-3xl text-white font-black text-center p-2 sm:text-5xl mt-5'><BlockHighlight color='green'>Art</BlockHighlight></h1>
-      <div className='m-3 z-1 align-middle w-5/6 bg-gradient-to-r from-pink via-green to-purple shadow-lg p-1 md:w-3/4 mx-auto md:mt-20 md:p-1'>
-        <p className='bg-white px-5 pb-10 pt-5 -mt-2 -ml-2 font-secondary text-sm text-${colour} text-base text-justify sm:text-lg lg:flex-row lg:px-20'>Art and design has played a big part of my life. Whilst I love the technical, logical side of my past work experience, I want to create a career where I&apos;m able to work creatively to solve problems, whilst exploring new designs and concepts.</p>
+      <h1 className='font-primary text-3xl text-white font-black text-center p-2 sm:text-5xl mt-10'><BlockHighlight color='green'>Art</BlockHighlight></h1>
+      <div className='m-3 z-1 align-middle w-5/6 bg-gradient-to-r from-pink via-green to-purple shadow-lg p-1 md:w-3/4 mx-auto md:mt-10'>
+        <p className='bg-white p-5 -mt-2 -ml-2 font-secondary text-${colour} text-sm text-justify sm:text-lg lg:flex-row lg:px-20 '>Art and design has played a big part of my life. Whilst I love the technical, logical side of my past work experience, I want to create a career where I&apos;m able to work creatively to solve problems, whilst exploring new designs and concepts.</p>
       </div>
       <PhotoGallery />
     </div>
